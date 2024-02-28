6a80068b54bf66ad97b43e3beb02d1b708b64695
AllanMogley <allanmogley@gmail.com>
2024-02-29 00:53:35 +0300

    Cleaned Folders

--- 09 Simple ML Works/Project-01-Housing_Prediction/Data/Data_Extract.py
+++ 09 Simple ML Works/Project-01-Housing_Prediction/Data/Data_Extract.py
@@ -3,13 +3,13 @@ import pandas as pd
 import tarfile
 import urllib.request
 def load_housing_data():
-    tarball_path = Path("datasets/housing.tgz")
+    tarball_path = Path("housing.tgz")
     if not tarball_path.is_file():
         Path("datasets").mkdir(parents=True, exist_ok=True)
         url = "https://github.com/ageron/data/raw/main/housing.tgz"
         urllib.request.urlretrieve(url, tarball_path)
         with tarfile.open(tarball_path) as housing_tarball:
             housing_tarball.extractall(path="datasets")
-    return pd.read_csv(Path("datasets/housing/housing.csv"))
+    return pd.read_csv(Path("housing.csv"))
 
 housing = load_housing_data()
