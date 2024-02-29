c54aebb1e0bdbabc3d70e5601ea5f3e3741d8150
AllanMogley <allanmogley@gmail.com>
2024-02-29 18:48:27 +0300

    Trained test split

---
+++ 09 Simple ML Works/Project-01-Housing_Prediction/02 - Exploratory/exploratory.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from pathlib import Path
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
+import numpy as np
+
+housing = pd.read_csv(Path('../Data/housing.csv'))
+
+housing['income_cat'] = pd.cut(housing['median_income']),
+                        bins = [1, 1.5, 3, 4.5, 6, np.inf],
+                        labels = [1, 2, 3, 4, 5]
+
+strat_train_set, strat_test_set = train_test_split(
+        housing, test_size = 0.2,
+        stratify = housing['income_cat'],
+        random_state = 42)
+
+strat_train_set.to_csv(Path('train_Set.csv'))
+strat_test_set.to_csv(Path('test_Set.csv'))
