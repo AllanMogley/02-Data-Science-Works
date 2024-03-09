018cb0ac42fbed1ca10d62d5b4ab03eff665658b
AllanMogley <allanmogley@gmail.com>
2024-03-10 00:00:49 +0300

    Added Comments

--- 09 Simple ML Works/Project-01-Housing_Prediction/02 - Exploratory/exploratory.py
+++ 09 Simple ML Works/Project-01-Housing_Prediction/02 - Exploratory/exploratory.py
@@ -5,6 +5,7 @@ import numpy as np
 
 housing = pd.read_csv(Path('../Data/housing.csv'))
 
+# Creste Bins for effective splitting
 housing['income_cat'] = pd.cut(housing['median_income'],
         bins = [0., 1.5, 3.0, 4.5, 6., np.inf],
         labels = [1, 2, 3, 4, 5])
